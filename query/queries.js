223258d13618a7871f97d3436a3c25f0bcffd112
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-09-10 13:51:41 +0200

    10/32

--- query/queries.js
+++ query/queries.js
@@ -6,27 +6,34 @@ db.restaurants.find({}, { _id: 0, restaurant_id: 1, name: 1 });
 
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
+db.restaurants.find({}, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
+db.restaurants.find({}, { _id: 0, restaurant_id: 1, name: 1, borough: 1, "address.zipcode": 1 });
 
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
+db.restaurants.find({ borough: "Bronx" }, { _id: 0 });
 
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
+db.restaurants.find({ borough: "Bronx" }, { _id: 0 }).limit(5);
 
 
 // 7. Mostrar el pròxims 5 restaurants després de saltar els primers 5 del Bronx.
+db.restaurants.find({ borough: "Bronx" }, { _id: 0 }).skip(5).limit(5);
 
 
 // 8. Trobar els restaurants amb un score de més de 90.
+db.restaurants.find({ "grades.score": { $gt: 90 } }, { _id: 0 });
 
 
 // 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
-
+db.restaurants.find({ "grades.score": { $gt: 80, $lt: 100 } }, { _id: 0 });
 
 // 10. Trobar els restaurants amb longitud menor que -95.754168.
+db.restaurants.find({ "location.coordinates.0": { $lt: -95.754168 } }, { _id: 0 });
 
 
 // 11. Trobar restaurants que no preparen 'American', amb qualificació > 70 i longitud < -65.754168.
