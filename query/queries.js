15f9c6c64740d117f33bce5c045c3c50bba9fcd9
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-09-10 13:26:11 +0200

    2first queries testing the sistem

--- query/queries.js
+++ query/queries.js
@@ -1,7 +1,8 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find({}, { _id: 0 });
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
+db.restaurants.find({}, { _id: 0, restaurant_id: 1, name: 1 });
 
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
