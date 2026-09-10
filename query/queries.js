4f80688fa8f28ca8f1949f0b4e636035da57be63
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-09-10 13:55:14 +0200

    9 fixed

--- query/queries.js
+++ query/queries.js
@@ -30,7 +30,8 @@ db.restaurants.find({ "grades.score": { $gt: 90 } }, { _id: 0 });
 
 
 // 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
-db.restaurants.find({ "grades.score": { $gt: 80, $lt: 100 } }, { _id: 0 });
+db.restaurants.find({ grades: { $elemMatch: { score: { $gt: 80, $lt: 100 } } } }, { _id: 0 });
+
 
 // 10. Trobar els restaurants amb longitud menor que -95.754168.
 db.restaurants.find({ "location.coordinates.0": { $lt: -95.754168 } }, { _id: 0 });
