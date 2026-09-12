e96ce37ec004ea36e5a24aab176cee0c089a0c83
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-09-12 17:31:16 +0200

    Adding Indexes to improve the performance

--- query/indexes.js
+++ query/indexes.js
@@ -1,2 +1,6 @@
 //Use this file to define indexes for your queries
 db.restaurants.createIndex({ "location": "2dsphere" });
+db.restaurants.createIndex({ "borough": 1 });
+db.restaurants.createIndex({ "cuisine": 1 });
+db.restaurants.createIndex({ "name": 1 });
+db.restaurants.createIndex({ "grades.score": 1 });
